7643ee17bce4f63f38c15147c4a513f640a2f29f
santahora <155487474+santahora@users.noreply.github.com>
2024-01-04 16:08:16 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -21,4 +21,30 @@ with st.sidebar:
     #     st.write(df["Segunda"][0])
 
 # if dom:
-st.write(df[df["Dia"] == dia_selecionado].drop(['ID missa', 'Arquidiocese', 'Decanato', 'Cidade', 'Observação'], axis=1).sort_values(by='Horário'))
+df_cut = df[df["Dia"] == dia_selecionado].drop(['ID missa', 'Arquidiocese', 'Decanato', 'Cidade', 'Observação'], axis=1).sort_values(by='Horário')
+# st.write(df_cut)
+
+# st.write(df_cut['Horário'].unique())
+
+# for horario in df_cut['Horário'].unique():
+#     st.write(horario)
+#     for paroquia in df_cut[df_cut['Horário'] == horario]['Paróquia']:
+#         st.write(paroquia)
+
+lista_tabs = []
+
+lista_tabs = st.tabs(list(df_cut['Horário'].unique()))
+
+# for tab in lista_horarios:
+#     with tab:
+#         st.write(type(str(tab)))
+#         for horario in df_cut['Horário'].unique():
+#             st.write(horario)
+#             for paroquia in df_cut[df_cut['Horário'] == horario]['Paróquia']:
+#                 st.write(paroquia)
+
+for tab, horario in zip(lista_tabs, df_cut['Horário'].unique()):
+    with tab:
+        st.write(horario)
+        for paroquia in df_cut[df_cut['Horário'] == horario]['Paróquia'].sort_values():
+            st.write(paroquia)
